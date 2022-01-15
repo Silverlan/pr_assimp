89ec89f639217457738fdeb868783e630d6b7d0c
Silverlan <silverlann@gmail.com>
2022-01-15 12:05:06 +0100

    Initial commit

---
+++ include/pr_assimp.hpp
@@ -0,0 +1,6 @@
+#ifndef __PR_ASSIMP_HPP__
+#define __PR_ASSIMP_HPP__
+
+
+
+#endif
---
+++ src/pr_assimp.cpp
@@ -0,0 +1,7 @@
+extern "C"
+{
+	void PRAGMA_EXPORT register_asset_format_handlers(assetManager,assetType)
+	{
+		
+	}
+};
